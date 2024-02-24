bd84edce2a1b9412f0c2356aa5f1f793a0292043
Madhan Kumar <106460217+Madhanjetti@users.noreply.github.com>
2024-02-24 10:18:51 +0530

    Add files via upload

---
+++ srcmainjava/StudentRepo.java
@@ -0,0 +1,10 @@
+//interface file 
+package com.example.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.entity.Student;
+
+public interface StudentRepo extends JpaRepository<Student, Long> {
+
+}
